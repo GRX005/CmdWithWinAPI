25835012eea433b058b664cda94d16bbc8155634
GRX005 <fodisoma@gmail.com>
2024-08-31 20:11:48 +0200

    Add project files.

---
+++ WinApi2/main.cpp
@@ -0,0 +1,36 @@
+#include <Windows.h>
+#include <iostream>
+
+using namespace std;
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
+{
+	AllocConsole();
+	FILE* pFile;
+
+	freopen_s(&pFile, "conin$", "r", stdin);
+	freopen_s(&pFile, "conout$", "w", stdout);
+	freopen_s(&pFile, "conout$", "w", stderr);
+
+
+	cout.sync_with_stdio(false);
+	SetConsoleTitle(TEXT("Console"));
+	cout << "Console opened." << endl;
+
+	int code = MessageBoxA(NULL, "Random Asd?","Console" , MB_YESNO | MB_ICONQUESTION);
+
+	switch (code)
+	{
+	case IDYES:
+		cout << "Pressed Yes." << endl;
+		system("pause");
+		break;
+	case IDNO:
+		cout << "Pressed No."<<endl;
+		break;
+	default:
+		break;
+	}
+
+	
+	return 0;
+}
